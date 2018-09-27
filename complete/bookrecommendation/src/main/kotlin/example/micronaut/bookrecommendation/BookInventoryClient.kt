4120aef8c0005fe284a843309381d4ab505ae121
Iván López <lopez.ivan@gmail.com>
2018-09-27 10:15:59 +0200

    Change @Client to new package

--- complete/bookrecommendation/src/main/kotlin/example/micronaut/bookrecommendation/BookInventoryClient.kt
+++ complete/bookrecommendation/src/main/kotlin/example/micronaut/bookrecommendation/BookInventoryClient.kt
@@ -1,7 +1,7 @@
 package example.micronaut.bookrecommendation
 
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.client.Client
+import io.micronaut.http.client.annotation.Client
 import io.reactivex.Maybe
 
 import javax.validation.constraints.NotBlank
